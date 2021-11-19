Apply random masking only in training and minor hard-coded fixes

coltran/datasets.py
@@ -100,7 +100,7 @@ def get_gen_dataset(data_dir, batch_size):
   return tf_dataset
 
 
-def create_gen_dataset_from_images(image_dir, mask_dir):
+def create_gen_dataset_from_images(image_dir, mask_dir, train):
   """Creates a dataset from the provided directory."""
   def load_image(path):
     image_str = tf.io.read_file(path)
@@ -145,7 +145,10 @@ def create_gen_dataset_from_images(image_dir, mask_dir):
       # add the last image to the cube list 2 times, with a random moderate mask + as is
       elif ind[0] == 55:  # FIXME: Hard-coded indexes
         im_mask = cv2.imread(im)
-        curr_mask = cv2.imread(random.choice(mod_masks), 0)
+        if train:
+          curr_mask = cv2.imread(random.choice(mod_masks), 0)
+        else:
+          curr_mask = cv2.imread(mod_masks[0], 0)
         im_mask[curr_mask > 0] = 0
         im_mask[curr_mask == 0] = im_mask[curr_mask == 0]
         im_mask = cv2.cvtColor(im_mask, cv2.COLOR_BGR2RGB)
@@ -162,13 +165,15 @@ def create_gen_dataset_from_images(image_dir, mask_dir):
 
     # if the last image is not clear skip the area
     last_category = categorize_mask(mask)
-    if last_category != 'clear':
+    if train and last_category != 'clear':
       continue
 
     # save the randomly cloudy image for evaluation
     gen = Image.fromarray(cube[-1], mode='RGB')
-    gen.save('D:\\Timeseries_cropped_512\\gen_for_eval\\test\\' + os.path.basename(r) + '.jpeg')
-    # FIXME: hard-coded path for image save! change for train and test
+    if train:
+      gen.save('D:\\Timeseries_cropped_512\\gen_for_eval\\train\\' + os.path.basename(r) + '.jpeg')
+    else:
+      gen.save('D:\\Timeseries_cropped_512\\gen_for_eval\\test\\' + os.path.basename(r) + '.jpeg')
 
     files = tf.concat(cube, axis=2)  # creates tensors with size (256,256,T*3)
     # print(files)
@@ -241,7 +246,7 @@ def get_dataset(name,
     ds = get_imagenet(subset, read_config)
   elif name == 'custom':
     assert data_dir is not None
-    ds = create_gen_dataset_from_images(data_dir, mask_dir)
+    ds = create_gen_dataset_from_images(data_dir, mask_dir, train=train)
   else:
     raise ValueError(f'Expected dataset in [imagenet, custom]. Got {name}')
 

coltran/models/colorizer.py
@@ -83,7 +83,6 @@ class ColTranCore(tf.keras.Model):
   def call(self, inputs, training=True):
     # encodes grayscale (H, W) into activations of shape (H, W, 512).
     enc_inputs = inputs[:, :, :, 3:]  # FIXME: hard-coded inputs for less memory
-    # enc_inputs = inputs[:, :, :, :-3]
     z = self.encoder(enc_inputs, channel_index=None, training=training)
 
     if self.is_parallel_loss:
@@ -92,7 +91,6 @@ class ColTranCore(tf.keras.Model):
       # (1, 64, 64, 1, 512)
 
     dec_logits = self.decoder(inputs[:, :, :, :3], z, training=training)  # FIXME: hard-coded only the first image (clear)
-    # dec_logits = self.decoder(inputs[:, :, :, -3:], z, training=training)
     if self.is_parallel_loss:
       return dec_logits, {'encoder_logits': enc_logits}
     return dec_logits, {}
@@ -141,7 +139,6 @@ class ColTranCore(tf.keras.Model):
       aux_output = {}
 
     labels = labels[:, :, :, :3] #FIXME: hard-coded only the first image (the clear one)
-    # labels = labels[:, :, :, -3:]
     # quantize labels.
     labels = base_utils.convert_bits(labels, n_bits_in=8, n_bits_out=3)
 
@@ -168,8 +165,7 @@ class ColTranCore(tf.keras.Model):
   def sample(self, gray_cond, mode='argmax'):
     output = {}
 
-    z_gray = self.encoder(gray_cond[:, :, :, 3:], training=False)  # inputs without the ground-truth image
-    # z_gray = self.encoder(gray_cond[:, :, :, :-3], training=False)
+    z_gray = self.encoder(gray_cond[:, :, :, 3:], training=False)  #FIXME: inputs without the ground-truth image
     if self.is_parallel_loss:
       z_logits = self.parallel_dense(z_gray)
       parallel_image = tf.argmax(z_logits, axis=-1, output_type=tf.int32)

coltran/sample.py
@@ -193,9 +193,14 @@ def store_samples(data, config, logdir, gen_dataset=None):
         output = model.sample(gray_cond=curr_gray, mode=sample_mode)
       logging.info('Done sampling')
 
-      #current = curr_gray[:, :, :, :3]
-      #result = tf.unique_with_counts(tf.reshape(tf.abs(current[:] - tf.cast(output['bit_up_argmax'][:], tf.int32)), [-1]))
+      current = curr_gray[:, :, :, :3]
+      # check differences in pixel values between the ground truth and the generated sample image
+      result = tf.unique_with_counts(tf.reshape(tf.abs(current[:] - tf.cast(output['bit_up_argmax'][:], tf.int32)), [-1]))
+      # check differences in pixel values between the ground truth and the generated sample image in the area of masks
+      mask_result = tf.unique_with_counts(tf.reshape(tf.abs(current[curr_gray[:, :, :, -3:] == [0, 0, 0]] - tf.cast(output['bit_up_argmax'][curr_gray[:, :, :, -3:] == [0, 0, 0]], tf.int32)), [-1]))
+
       #print(result)
+      #print(mask_result)
 
       for out_key, out_item in output.items():
         curr_output[out_key].append(out_item.numpy())